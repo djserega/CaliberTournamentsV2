13e59abe61315174d165fef60de3fab32673448f
djserega <galyuk.sergey@gmail.com>
2022-04-19 22:18:40 +0300

    create logger class
    combined data to log-embeds

---
+++ src/CaliberTournamentsV2/Logger.cs
@@ -0,0 +1,67 @@
+﻿using DSharpPlus;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaliberTournamentsV2
+{
+    internal class Logger
+    {
+        private Builders.MessageBuilder? _logBuilder = default;
+
+        public Logger(Models.PickBans.PickBanMap? logsMaps)
+        {
+            LogsMaps = logsMaps;
+        }
+
+        Models.PickBans.PickBanMap? LogsMaps { get; } = default;
+
+        internal void CreateBuilderLogs(string description)
+        {
+            _logBuilder = new Builders.MessageBuilder()
+                .AddDescription($"Голосование команд: {description}");
+
+
+            if (LogsMaps != default)
+            {
+                Builders.Embeds embedsLogMaps = new Builders.Embeds()
+                    .Init()
+                    .AddDescription("Карты");
+
+                embedsLogMaps.AddField("Период", $"{LogsMaps.DateStart.GetFormattedTime()}-{LogsMaps.DateEnd.GetFormattedTime()}");
+                embedsLogMaps.AddField("Пик", LogsMaps.GetFormatterDetailed(Models.PickBanType.pick));
+                embedsLogMaps.AddField("Бан", LogsMaps.GetFormatterDetailed(Models.PickBanType.ban));
+
+                _logBuilder.AddEmbed(embedsLogMaps.GetEmbed());
+
+
+                foreach (Models.PickBans.PickBanDetailed itemMap in LogsMaps.PickBanDetailed)
+                {
+                    if (itemMap.Operators.ResultGenerated)
+                    {
+
+                        Builders.Embeds embedsLogOperators = new Builders.Embeds()
+                            .Init()
+                            .AddDescription($"Карта {Formatter.Bold(itemMap.PickBanName)}");
+
+                        embedsLogOperators.AddField("Период", $"{itemMap.Operators.DateStart.GetFormattedTime()}-{itemMap.Operators.DateEnd.GetFormattedTime()}");
+                        embedsLogOperators.AddField("Пик", itemMap.Operators.GetFormatterDetailed(Models.PickBanType.pick));
+                        embedsLogOperators.AddField("Бан", itemMap.Operators.GetFormatterDetailed(Models.PickBanType.ban));
+
+                        _logBuilder.AddEmbed(embedsLogOperators.GetEmbed());
+                    }
+                }
+            }
+        }
+
+        internal void SendMessage(ulong idChannel, ulong idMessage)
+        {
+            if (_logBuilder == default)
+                return;
+
+            MessageQueue.Add(idChannel, _logBuilder.GetMessage(), idMessage);
+        }
+    }
+}
